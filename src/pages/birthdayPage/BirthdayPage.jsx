d227b1e389eaea34651a6322b1468b4ce794b9fd
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-06-18 13:51:34 +0100

    Editing

--- src/pages/birthdayPage/BirthdayPage.jsx
+++ src/pages/birthdayPage/BirthdayPage.jsx
@@ -11,7 +11,7 @@ export default class BirthdayPage extends Component {
       <img src={mydaddy} alt="daddy" width="300" height="300"/>
     </div>
     <h1 class="shimmer">Happy Father's Day</h1>
-    <p>Any man can be a father, but it takes someone special to be a dad. A father is neither an anchor to hold us back, nor a sail to take us there, but a guiding light whose love shows us the way. I am so luck to have you as a Dad and I cherish this priveledge every day. Happy fathers day sir</p>
+    <p>Any man can be a father, but it takes someone special to be a dad. A father is neither an anchor to hold us back, nor a sail to take us there, but a guiding light whose love shows us the way. I am so lucky to have you as a Dad and I cherish this priveledge every day. Happy fathers day sir</p>
   </div>
   <div id="overlay">From Israel</div>
           </div>
